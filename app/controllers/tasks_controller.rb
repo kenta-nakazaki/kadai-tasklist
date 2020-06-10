41ef1b41c8f37be7afc8b1556ff43a35138b43a4
kenta.nakazaki <nakazaki.kenta@lmi.ne.jp>
2020-06-10 08:31:54 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,16 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in, only: [:show, :edit, :new]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:destroy, :show, :edit]
     
     
     def index
-        @tasks = Task.all
+        if logged_in?
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end 
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
 
         if @task.save
           flash[:success] = 'Task が正常に投稿されました'
@@ -19,19 +22,19 @@ class TasksController < ApplicationController
     end 
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end 
     
     def edit
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(id: params[:id])
     end 
     
     def show
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(id: params[:id])
     end 
     
     def update
-        @task = Task.find(params[:id])
+        @task = current_user.tasks.find_by(id: params[:id])
         
         if @task.update(task_params)
             flash[:success] = 'Taskは正常に更新されました'
@@ -43,11 +46,9 @@ class TasksController < ApplicationController
     end 
     
     def destroy
-        @task = Task.find(params[:id])
         @task.destroy
-        
         flash[:success]= 'Taskは正常に削除されました'
-        redirect_to tasks_url
+        redirect_to root_url
     end 
     
     private
@@ -56,4 +57,10 @@ class TasksController < ApplicationController
         params.require(:task).permit(:content, :status)
     end
     
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end 
 end
